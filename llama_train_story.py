aa6bcc2dd67adc294559ad99970c44fdfb5dca19
Saeyoon Oh <saeyoon17@kaist.ac.kr>
2023-08-07 15:11:03 +0900

    fix path

--- llama_train_story.py
+++ llama_train_story.py
@@ -49,7 +49,7 @@ def train():
     parser.add_argument("--lora_alpha", type=int, default=8)
     parser.add_argument("--lora_dropout", type=float, default=0.05)
     parser.add_argument("--lr", type=float, default=2e-4)
-    parser.add_argument("--num_epoch", type=int, default=10)
+    parser.add_argument("--num_epoch", type=int, default=50)
     parser.add_argument("--save_interval", type=int, default=5)
     args = parser.parse_args()
 
