459281b62138132cb097f8aa369e766c54c148fd
Saeyoon Oh <saeyoon17@kaist.ac.kr>
2023-08-09 13:41:17 +0900

    auto device map

--- inference.py
+++ inference.py
@@ -12,8 +12,7 @@ from peft import LoraConfig, TaskType, get_peft_model
 peft_model_id = "/ckpt/"
 max_memory = {0: "80GIB", 1: "80GIB", "cpu": "30GB"}
 config = PeftConfig.from_pretrained(peft_model_id)
-print(config.base_model_name_or_path)
-model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map="auto", max_memory=max_memory, torch_dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, device_map="auto", torch_dtype=torch.float16)
 model = PeftModel.from_pretrained(model, peft_model_id, device_map="auto", max_memory=max_memory)
 model.eval()
 tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path, legacy=False)
